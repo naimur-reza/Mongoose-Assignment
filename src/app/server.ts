d36893b4e9dbe80303effb1d27b5237b0c27a5f4
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-23 00:58:53 +0600

    Add dotenv

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 import { app } from "./app";
-
-const port = 5000;
+import config from "./config";
 
 async function main() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/assignment");
+    await mongoose.connect(config.database_url_local!);
     console.log("Database connected!");
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
