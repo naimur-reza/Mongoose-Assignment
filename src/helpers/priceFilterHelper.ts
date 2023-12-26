72794620412b03879b485203850a7327a3e98af3
Naimur Reza <naimur.rezaa@gmail.com>
2023-12-26 11:12:20 +0600

    Refactor priceFilterHelper to handle undefined minPrice and maxPrice values

--- src/helpers/priceFilterHelper.ts
+++ src/helpers/priceFilterHelper.ts
@@ -2,7 +2,7 @@ import { Query } from "mongoose";
 import IQueryObj from "../types/IQueryObj";
 
 const priceFilterHelper = <T>(model: Query<T[], T>, query: IQueryObj) => {
-  if (query.minPrice && query.maxPrice) {
+  if (query.minPrice !== undefined && query.maxPrice !== undefined) {
     const queryObj = {
       $lte: Number(query.maxPrice),
       $gte: Number(query.minPrice),
